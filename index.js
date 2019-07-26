0652099def4aede09f65149ed09b44d9a4285e60
Fábio <videiraft@gmail.com>
2019-07-26 19:23:41 +0200

    feat: enable cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const usersRouter = require('./routes/usersRouter');
 
 dotenv.config({ path: './config.env' });
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/users', usersRouter);
 
